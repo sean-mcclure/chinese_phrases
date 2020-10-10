05d74f07e495f81e2b8a272051c514b3c577e2d8
Sean McClure <seanaaron100@gmail.com>
2020-10-10 14:37:37 +0000

    adding build

---
+++ chinese_app/build/precache-manifest.249fc48e7f192aa4fc0b9065cb9dbe5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5a68bacac8d9a51a8302f241a107b6",
+    "url": "/chinese/index.html"
+  },
+  {
+    "revision": "9dfc62d37c42ec92d018",
+    "url": "/chinese/static/css/main.df5d235a.chunk.css"
+  },
+  {
+    "revision": "499a81bd57ff02f0d47b",
+    "url": "/chinese/static/js/2.866a6eca.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/chinese/static/js/2.866a6eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dfc62d37c42ec92d018",
+    "url": "/chinese/static/js/main.31e31238.chunk.js"
+  },
+  {
+    "revision": "dbbb658c7450ce060ccb",
+    "url": "/chinese/static/js/runtime-main.fbca1b8b.js"
+  }
+]);
